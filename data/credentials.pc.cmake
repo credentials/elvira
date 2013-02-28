6b9a65932837b16c06d290462ad0260ed8c2489a
Pim Vullers <pim@vullersmail.nl>
2013-02-28 11:58:10 +0100

    Added CMake build system for easy building and packaging

---
+++ data/credentials.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@PREFIX@
+exec_prefix=@DOLLAR@{prefix}
+libdir=@DOLLAR@{prefix}/lib
+includedir=@DOLLAR@{prefix}/include
+
+Name: @PKG_NAME@
+Description: @PKG_DESCRIPTION@
+Version: @PKG_VERSION@
+Libs: -L@DOLLAR@{libdir} -l@PKG_NAME@
+Cflags: -I@DOLLAR@{includedir}/${PKG_NAME}
+Requires: @PKG_DEPS@ credentials_crypto
